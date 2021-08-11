cf41f570000e7019964e1efb1369606acd6adaeb
lucasalcantarap <lucaspe0685@gmail.com>
2021-08-11 19:56:53 -0300

    adicionado procile erequirements

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^p*=s)l4cx@rr)qnmde&^=03^kpt!&)7hu=cq6!@ci(ke^au6_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
